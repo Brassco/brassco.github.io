514ebd0815e34146a8378bf6f96b6f6af2823036
Brassco <tonny.brassco@gmail.com>
2022-11-22 11:35:32 +0200

    updated nav menu, updated testimonials, updated portfolio

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,36 @@
 const button = document.getElementById('navbar-button');
 const mobileMenu = document.querySelector('.mobile-menu-container');
+const sections = document.querySelectorAll('.section');
 
-button.addEventListener('click', () => {
-    mobileMenu.classList.add('show-mobile-menu')
+document.addEventListener('click', (event) => {
+    if (event.target?.id !== "mobile-menu-button") {
+        mobileMenu.classList.remove('show-mobile-menu')
+    }
 })
 
-function scrollActive() {
+button.addEventListener('click', () => {
+    mobileMenu.classList.toggle('show-mobile-menu')
+})
+
+function scrollActive(ev) {
     const scrollY = pageYOffset;
     sections.forEach( section => {
         const sectionHeight = section.offsetHeight;
         const sectionTop = section.offsetTop - 50;
         const sectionId = section.getAttribute('id');
 
-    if (sectionId) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        console.log('document end')
+        document.querySelector('.nav-item a[href*=contact]').classList.add('active')
+    }
 
+    if (sectionId) {
+        document.querySelector('.nav-item a[href*=' + sectionId + ']').classList.remove('active')
+        
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+            document.querySelector('.nav-item a[href*=' + sectionId + ']').classList.add('active')
         } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            document.querySelector('.nav-item a[href*=' + sectionId + ']').classList.remove('active')
         }
     }
     })
